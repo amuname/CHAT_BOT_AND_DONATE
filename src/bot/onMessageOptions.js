ebaaf64d375134b480adce4b84d12f525ac17f97
amuname <74045707+amuname@users.noreply.github.com>
2021-08-29 10:04:49 -0700

    Create onMessageOptions.js

---
+++ src/bot/onMessageOptions.js
@@ -0,0 +1,41 @@
+const {inlineButtonsKeyBoard,start_buttons} = require('./buttons.js')
+
+async function onMessagePromiseOptions(text,chat_id,ctx,res,rej){
+	
+	const start_regexp = /\/start .\d+/,
+	start = /\/start/
+
+	switch (true){
+				case start.test(text) : 
+					const buttons = start_buttons
+
+					const keyboard = await inlineButtonsKeyBoard(buttons)
+
+					// await bd.write message?)) or smth
+					// await this.telegram.callApi('sendMessage', { chat_id, text:'asa'})
+					console.log('\r\ndvsdvsvw\r\n')
+
+					res(await this.telegram.sendMessage(chat_id,'hello there',keyboard))
+					//
+					break
+				case start_regexp.test(text) : 
+					//
+					//
+					break
+				case /a/.test(text) : 
+					//
+					res(this.telegram.sendMessage(chat_id,'wowowow'))
+					//
+					break
+				default :
+					return
+					// console.log('this is default condition')
+			}
+
+}
+
+
+
+
+
+module.exports = onMessagePromiseOptions
