1ce90dd3755e4ad07d15ed7736da2a5454c44a2f
amuname <74045707+amuname@users.noreply.github.com>
2021-08-29 10:04:55 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+'use strict'
 const Bot = require('./bot/bot.js')
 const token = require('./bot/token.js')
 const bd = require('./bd/mongo.js')
@@ -5,13 +6,15 @@ const bd = require('./bd/mongo.js')
 
 const BOT = new Bot(token)
 
-console.log(BOT)
 
-BOT.on('message',function(ctx){
-	console.log(ctx.update.message)
-})
+// BOT.getUpdates().then(e=>console.log(e))
+
+BOT.on('message',
+	(ctx)=>BOT.onMessage(ctx)
+)
 BOT.on('callback_query',function(){
 	
 })
 
 BOT.launch()
+
