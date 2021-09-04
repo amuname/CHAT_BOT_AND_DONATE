ece2b73747743e3a5a5753f188edd5469f2dccbc
amuname <74045707+amuname@users.noreply.github.com>
2021-09-04 08:59:50 -0700

    Update bot.js

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -38,7 +38,7 @@ module.exports  = {
 
 	// callback methods
 
-	onMessage(ctx){
+	onMessage(ctx,bot){
 		const message_object = ctx.update.message
 		
 		//!!!!!!!!!!!!!!!!
@@ -53,40 +53,60 @@ module.exports  = {
 		// this method send message and can be usefull in server callbacks
 		// 
 		// console.log('\r\nOnmessage',text)
-		return new Promise(async function() {
+		return new Promise(async (res,rej)=> {
 			const start_regexp = /\/start .\d+/,
 			start = /\/start/
 
 			switch (true){
-						case start.test(text) : 
-							const buttons = start_buttons
+				case start.test(text) : 
+					const buttons = start_buttons
 
-							await mLogic.bdGet({id:ctx.from.id}).then(async (e)=>{
+					const dbResponse =await mLogic.bdAddUser({
+						user:{
+							id:sender_id,
+							lang:lang_code,
+							chat_status:'bot',
+						    conversations_with: {
+						        ['bot']: {
+						            messages_sended: [
+						            	{
+						            		usr_msg:text,
+						            		time:date,
+						            	},
+						            ]
+						        },
+							},
+						}
+					})
+					if(dbResponse=='OK'){
 
-								const keyboard = await inlineButtonsKeyBoard(buttons)
+						const keyboard = await inlineButtonsKeyBoard(buttons)
 
-								// await bd.write message?)) or smth
-								// await this.telegram.callApi('sendMessage', { chat_id, text:'asa'})
-								console.log('mongo func\r\n','\r\n'+e+'\r\n')
+						// console.log('mongo func\r\n','shood be "OK"\r\n'+dbResponse+'\r\n')
 
-								res(await this.telegram.sendMessage(chat_id,'hello there',keyboard))
+						res(await bot.telegram.sendMessage(sender_id,'hello there',keyboard))
 
-							})
-							//
-							break
-						case start_regexp.test(text) : 
-							//
-							//
-							break
-						case /a/.test(text) : 
-							//
-							res(this.telegram.sendMessage(chat_id,'wowowow'))
-							//
-							break
-						default :
-							return
-							// console.log('this is default condition')
+					} 
+					if(dbResponse=='registred'){
+						res(await bot.telegram.sendMessage(sender_id,'Use buttons or /help command'))
 					}
+							
+					break
+				case start_regexp.test(text) : 
+							//
+							//
+					break
+				case /nothing/.test(text) : 
+							//
+					// const keyboard = await inlineButtonsKeyBoard(start_buttons)
+					// const response = await mLogic.bdGetUser(sender_id)
+					// res(bot.telegram.sendMessage(chat_id,`u: ${response}`,keyboard))
+							//
+					break
+				default :
+					res(await bot.telegram.sendMessage(sender_id,'Use buttons or /help command'))
+					// console.log('this is default condition')
+			}
 		})
 
 	},
