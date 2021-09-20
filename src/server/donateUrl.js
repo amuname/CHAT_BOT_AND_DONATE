f93429db55a90f3491165bc0c541d8afd9e7d439
amuname <74045707+amuname@users.noreply.github.com>
2021-09-20 13:38:55 -0700

    Create donateUrl.js

---
+++ src/server/donateUrl.js
@@ -0,0 +1,9 @@
+
+module.exports = function(price,user,time){
+	const domain = ''
+	const url = 'http'+'s'+'://'+domain+'/'
+	const receiver =4100116759492012
+	const target = /*На развитие канала*/ '%D0%9D%D0%B0%20%D1%80%D0%B0%D0%B7%D0%B2%D0%B8%D1%82%D0%B8%D0%B5%20%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%B0'
+	const successURL = url+'success'+user+time+'-'+price
+	return `https://yoomoney.ru/quickpay/confirm.xml?receiver=${receiver}&successURL=${successURL}&label=&quickpay-form=donate&is-inner-form=true&referer=${referer}&targets=${target}&comment=%09&sum=${price}&paymentType=AC`
+} 
