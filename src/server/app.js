f694e4a15f6cf249240186a90cccea4b9d2d4c5b
amuname <74045707+amuname@users.noreply.github.com>
2021-09-26 15:19:35 -0700

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,15 @@
-const app = require('express')
+'use strict'
+const express = require('express')
 const donateUrlConstructor = require('./donateUrl.js')
+const bd = require('./../bd/mongo.js')
+const app = express()
+
+app.get('/d/',(req,res,next)=>{
+	console.dir(req.url)
+	const url = new URL(req.url)
+	res.send(url)
+})
 
 
 
-module.exports = server
+app.listen(3000)
