3ea4b95ff848152a3ab7fafc7c7c59e0f95126cb
amuname <74045707+amuname@users.noreply.github.com>
2021-10-23 12:57:19 -0700

    Update app.js

--- src/server/app.js
+++ src/server/app.js
@@ -4,12 +4,29 @@ const donateUrlConstructor = require('./donateUrl.js')
 const bd = require('./../bd/mongo.js')
 const app = express()
 
-app.get('/d/',(req,res,next)=>{
+app.get(/d\/.+/,async(req,res,next)=>{
 	console.dir(req.url)
-	const url = new URL(req.url)
-	res.send(url)
+	const donation = await getDonationUrl(req.url) 
+	const user = donation.user_id
+	const amount = donation.amount
+	const succses_url = donation.succses_url
+	const redirect = await bd.donateUrlConstructor(amount,succses_url)
+	res.redirect(301, redirect)
 })
 
+app.get(/s\/.+s/,(req,res,next)=>{
+	console.dir(req.url)
+	await bd.getSuccessUr(req.url) 
+	
+	res.send('thanks')
+})
+
+app.get(/.+/,(req,res,next)=>{
+	console.dir(req.url)
+	// await findUrlAndUpdate 
+	// req.url
+	res.send('fuck u')
+})
 
 
 app.listen(3000)
